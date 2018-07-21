51006b62e6b2c9bd63c5e872ab3c8d6b6574772b
David Cunha <davidgoncalvescunha@gmail.com>
2018-07-21 18:03:37 +0100

    feat: first commit

---
+++ assets/scripts/main.js
@@ -0,0 +1,71 @@
+/* eslint-disable func-names */
+/* ========================================================================
+ * DOM-based Routing
+ * Based on http://goo.gl/EUTi53 by Paul Irish
+ *
+ * Only fires on body classes that match. If a body class contains a dash,
+ * replace the dash with an underscore when adding it to the object below.
+ *
+ * .noConflict()
+ * The routing is enclosed within an anonymous function so that you can
+ * always reference jQuery with $, even when in .noConflict() mode.
+ * ======================================================================== */
+
+(function ($) {
+  // Use this variable to set up the common and page specific functions. If you
+  // rename this variable, you will also need to rename the namespace below.
+  var Router = {
+    // All pages
+    common: {
+      init() {
+        // JavaScript to be fired on all pages
+      },
+      finalize() {
+        // JavaScript to be fired on all pages, after page specific JS is fired
+      },
+    },
+    // Home page
+    home: {
+      init() {
+        // JavaScript to be fired on the home page
+      },
+      finalize() {
+        // JavaScript to be fired on the home page, after the init JS
+      },
+    },
+  };
+
+  // The routing fires all common scripts, followed by the page specific scripts.
+  // Add additional events for more control over timing e.g. a finalize event
+  const UTIL = {
+    fire(func, funcname, args) {
+      let fire;
+      const namespace = Router;
+      const funcnameArg = funcname === undefined ? 'init' : funcname;
+      fire = func !== '';
+      fire = fire && namespace[func];
+      fire = fire && typeof namespace[func][funcnameArg] === 'function';
+
+      if (fire) {
+        namespace[func][funcnameArg](args);
+      }
+    },
+    loadEvents() {
+      // Fire common init JS
+      UTIL.fire('common');
+
+      // Fire page-specific init JS, and then finalize JS
+      $.each(document.body.className.replace(/-/g, '_').split(/\s+/), (i, classnm) => {
+        UTIL.fire(classnm);
+        UTIL.fire(classnm, 'finalize');
+      });
+
+      // Fire common finalize JS
+      UTIL.fire('common', 'finalize');
+    },
+  };
+
+  // Load Events
+  $(document).ready(UTIL.loadEvents);
+  /* eslint-disable no-undef */
+}(jQuery)); // Fully reference jQuery after this point.
